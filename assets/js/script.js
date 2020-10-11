7a78edd1ddfadf023df34bf0f67a80aba171e366
singhkunal2050 <singhkunal2050@gmail.com>
2020-10-11 19:01:01 +0530

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,57 @@
 (function($,sr){
-
+  
   // debouncing function from John Hann
   // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
   var debounce = function (func, threshold, execAsap) {
-      var timeout;
-
-      return function debounced () {
-          var obj = this, args = arguments;
-          function delayed () {
-              if (!execAsap)
-                  func.apply(obj, args);
-              timeout = null;
-          };
-
-          if (timeout)
-              clearTimeout(timeout);
-          else if (execAsap)
-              func.apply(obj, args);
-
-          timeout = setTimeout(delayed, threshold || 100);
+    var timeout;
+    
+    return function debounced () {
+      var obj = this, args = arguments;
+      function delayed () {
+        if (!execAsap)
+        func.apply(obj, args);
+        timeout = null;
       };
+      
+      if (timeout)
+      clearTimeout(timeout);
+      else if (execAsap)
+      func.apply(obj, args);
+      
+      timeout = setTimeout(delayed, threshold || 100);
+    };
   }
   // smartresize 
   jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
-
+  
 })(jQuery,'smartresize');
 
 var $ = jQuery;
 
 
 (function(){
-
-
+  
+  
   ///////////////////////////////
   // Set Home Slideshow Height
   ///////////////////////////////
-
+  
   function setHomeBannerHeight() {
     var windowHeight = jQuery(window).height(); 
     jQuery('#header').height(windowHeight);
   }
-
+  
   ///////////////////////////////
   // Center Home Slideshow Text
   ///////////////////////////////
-
+  
   function centerHomeBannerText() {
-      var bannerText = jQuery('#header > .center');
-      var bannerTextTop = (jQuery('#header').actual('height')/2) - (jQuery('#header > .center').actual('height')/2) - 40;   
-      bannerText.css('padding-top', bannerTextTop+'px');    
-      bannerText.show();
+    var bannerText = jQuery('#header > .center');
+    var bannerTextTop = (jQuery('#header').actual('height')/2) - (jQuery('#header > .center').actual('height')/2) - 40;   
+    bannerText.css('padding-top', bannerTextTop+'px');    
+    bannerText.show();
   }
-
+  
   function setHeaderBackground() {    
     var scrollTop = jQuery(window).scrollTop(); // our current vertical position from the top 
     
@@ -61,18 +61,18 @@ var $ = jQuery;
       jQuery('#header .top').removeClass('solid');    
     }
   }
-
-
-
-
+  
+  
+  
+  
   ///////////////////////////////
   // Initialize
   ///////////////////////////////
-
+  
   jQuery.noConflict();
   setHomeBannerHeight();
   centerHomeBannerText();
-
+  
   //Resize events
   jQuery(window).smartresize(function(){
     setHomeBannerHeight();
@@ -82,9 +82,9 @@ var $ = jQuery;
 })();
 
 
-  ///////////////////////////////
-  // Smooth Scroll
-  ///////////////////////////////
+///////////////////////////////
+// Smooth Scroll
+///////////////////////////////
 
 
 smoothScroll.init();
@@ -92,94 +92,96 @@ smoothScroll.init();
 
 
 
-  ///////////////////////////////
-  // Animate Css
-  ///////////////////////////////
+///////////////////////////////
+// Animate Css
+///////////////////////////////
 var $ = jQuery;
 
 function animationHover(element, animation){
-    element = $(element);
-    element.hover(
-        function() {
-            element.addClass('animated ' + animation);        
-        },
-        function(){
-            //wait for animation to finish before removing classes
-            window.setTimeout( function(){
-                element.removeClass('animated ' + animation);
-            }, 2000);         
-        });
-}
-
-$(document).ready(function(){
-    $('#scrollToContent').each(function() {
-        animationHover(this, 'pulse');
+  element = $(element);
+  element.hover(
+    function() {
+      element.addClass('animated ' + animation);        
+    },
+    function(){
+      //wait for animation to finish before removing classes
+      window.setTimeout( function(){
+        element.removeClass('animated ' + animation);
+      }, 2000);         
     });
-});
-
-
-
+  }
+  
+  $(document).ready(function(){
+    $('#scrollToContent').each(function() {
+      animationHover(this, 'pulse');
+    });
+  });
+  
+  
+  
   ///////////////////////////////
   // Header Fixed
   ///////////////////////////////
-
-
-
-var menu = $('#navigation');
-var origOffsetY = menu.offset().top;
-
-function scroll() {
-   if ($(window).scrollTop() >= origOffsetY) {
-       $('#navigation').addClass('nav-wrap');
-       $('#services').addClass('exp');
-       //$('.content').addClass('menu-padding');
-   } else {
-       $('#navigation').removeClass('nav-wrap');
-       $('#services').removeClass('exp');
-       //$('.content').removeClass('menu-padding');
-   }
-
-
-
-}
-
- document.onscroll = scroll;
-
-
+  
+  
+  
+  var menu = $('#navigation');
+  var origOffsetY = menu.offset().top;
+  
+  function scroll() {
+    if ($(window).scrollTop() >= origOffsetY) {
+      $('#navigation').addClass('nav-wrap');
+      $('#services').addClass('exp');
+      //$('.content').addClass('menu-padding');
+    } else {
+      $('#navigation').removeClass('nav-wrap');
+      $('#services').removeClass('exp');
+      //$('.content').removeClass('menu-padding');
+    }
+    
+    
+    
+  }
+  
+  document.onscroll = scroll;
+  
+  
   ///////////////////////////////
   // Testimonial Slide
   ///////////////////////////////
-
- $(document).ready(function() {
- 
-  $("#testimonial-container").owlCarousel({
- 
-      navigation : false, // Show next and prev buttons
-      slideSpeed : 700,
+  
+  $(document).ready(function() {
+    
+    $("#testimonial-container").owlCarousel({
+      navigation : true, // Show next and prev buttons
+      loop:true,
       paginationSpeed : 400,
       singleItem:true,
+      autoPlay:true,
+      slideSpeed:100,
+    });
+    
   });
- 
-});
-
-
+  
+  
   ///////////////////////////////
   // google map
   ///////////////////////////////
-
-function initialize()
-{
-var mapProp = {
-  center:new google.maps.LatLng(51.508742,-0.120850),
-  zoom:5,
-  mapTypeId:google.maps.MapTypeId.ROADMAP,
-  disableDefaultUI: true,
-  scrollwheel: false
-  };
-var map=new google.maps.Map(document.getElementById("googleMap")
-  ,mapProp);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
-
+  
+  function initialize()
+  {
+    var mapProp = {
+      center:new google.maps.LatLng(51.508742,-0.120850),
+      zoom:5,
+      mapTypeId:google.maps.MapTypeId.ROADMAP,
+      disableDefaultUI: true,
+      scrollwheel: false
+    };
+    var map=new google.maps.Map(document.getElementById("googleMap")
+    ,mapProp);
+  }
+  
+  google.maps.event.addDomListener(window, 'load', initialize);
+  
+  
+  
